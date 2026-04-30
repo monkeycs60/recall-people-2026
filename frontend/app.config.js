f9a3f22ec3995c2da94d964159c214f5ebc55b63
clementserizay <clement.serizay@affilae.com>
2026-04-30 23:06:04 +0200

    Bump iOS build number to 25

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -33,7 +33,7 @@ module.exports = {
     ios: {
       supportsTablet: false,
       bundleIdentifier: 'com.monkeycs60.recallpeople2026',
-      buildNumber: '24',
+      buildNumber: '25',
     },
     android: {
       adaptiveIcon: {
